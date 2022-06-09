52f3d13d673b1c8f84fa4b9c6b6e348961ebf02c
Afriyie Samuel <afriyiesamuel36@gmail.com>
2022-06-08 21:29:39 -0700

    worked on the nav popups

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -13,22 +13,67 @@ const Navbar = () => {
             </div>
             <ul className="navigation-list">
                 <li className="navigation-item active">
-                    <p>Home</p>
+                    <p className="nav-home">Home</p>
+                    <div className="home-options">
+                        <ul>
+                            <li className="home-option"><p>Home Design studio</p></li>
+                            <li className="home-option"><p>Home Web Hosting</p></li>
+                            <li className="home-option"><p>Home Support Desk</p></li>
+                            <li className="home-option"><p>Home Apps Landing</p></li>
+                            <li className="home-option"><p>Home Class Based Sass</p></li>
+                        </ul>
+                    </div>
                 </li>
                 <li className="navigation-item">
                     <p>About</p>
+                    <div className="about-options">
+                        <ul>
+                            <li className="home-option"><p>About Us</p></li>
+                            <li className="home-option"><p>About Company</p></li>
+                        </ul>
+                    </div>
                 </li>
                 <li className="navigation-item">
                     <p>Services</p>
+                    <div className="services-options">
+                        <ul>
+                            <li className="home-option"><p>Website designing</p></li>
+                            <li className="home-option"><p>Website development</p></li>
+                        </ul>
+                    </div>
                 </li>
                 <li className="navigation-item">
                     <p>Case Studies</p>
+                    <div className="case-options">
+                        <ul>
+                            <li className="home-option"><p>case study</p></li>
+                            <li className="home-option"><p>case study 01</p></li>
+                        </ul>
+                    </div>                    
                 </li>
                 <li className="navigation-item">
                     <p>Page</p>
+                    <div className="page-options">
+                        <ul>
+                            <li className="home-option"><p>Job Details</p></li>
+                            <li className="home-option"><p>Job Listings</p></li>
+                            <li className="home-option"><p>Our Team</p></li>
+                            <li className="home-option"><p>Pricing Plans</p></li>
+                            <li className="home-option"><p>Login</p></li>
+                            <li className="home-option"><p>Register</p></li>
+                            <li className="home-option"><p>FAQ'S</p></li>
+                        </ul>
+                    </div> 
                 </li>
                 <li className="navigation-item">
                     <p>Blog</p>
+                    <div className="blog-options">
+                        <ul>
+                            <li className="home-option"><p>Blog Grid</p></li>
+                            <li className="home-option"><p>Blog Single</p></li>
+                            <li className="home-option"><p>Our Blog</p></li>
+                        </ul>
+                    </div>
                 </li>
                 <li className="navigation-item">
                     <p>Contact</p>
